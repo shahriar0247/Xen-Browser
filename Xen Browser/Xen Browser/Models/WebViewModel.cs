1735d43b87daf0214f12ef5c03a526a68c8f88bd
Ahmed Shahriar <shahriar0247@gmail.com>
2022-04-19 08:09:50 +0600

    some updates back button etc

--- Xen Browser/Xen Browser/Models/WebViewModel.cs
+++ Xen Browser/Xen Browser/Models/WebViewModel.cs
@@ -9,27 +9,10 @@ namespace Xen_Browser.Models
     {
         public WebViewModel()
         {
-            loadurl_command = new Command(loadurl);
 
         }
 
-        public Command loadurl_command { get; }
-
-        public string URL;
-        public string url_bind
-        {
-            get => URL;
-            set
-            {
-                URL = value;
-                OnPropertyChanged();
-            }
-        }
-
-        void loadurl()
-        {
-            OnPropertyChanged(nameof(url_bind));
-        }
+     
 
     }
 }
--- Xen Browser/Xen Browser/View/WebViewView.xaml.cs
+++ Xen Browser/Xen Browser/View/WebViewView.xaml.cs
@@ -11,12 +11,66 @@ namespace Xen_Browser
 {
     public partial class MainPage : ContentPage
     {
+        public string URL;
+
         public MainPage()
         {
             InitializeComponent();
+            webview.Source = "https://www.google.com";
 
-            BindingContext = new WebViewModel();
+            BindingContext = this;
         }
 
+        public string webview_bind
+        {
+            set
+            {
+                input_url.Text = value;
+            }
+        }
+
+
+        private void input_url_Focused(object sender, FocusEventArgs e)
+        {
+            URL = input_url.Text;
+        }
+
+        private void load_btn_Clicked(object sender, EventArgs e)
+        {
+            webview.Source = input_url.Text;
+            URL = input_url.Text;
+
+        }
+
+        private void webview_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            URL = e.Url;
+            input_url.Text = e.Url;
+
+        }
+
+        private void webview_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            URL = e.Url;
+            input_url.Text = e.Url;
+
+        }
+
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (webview.CanGoBack)
+            {
+                webview.GoBack();
+
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
+
+            return true;
+
+        }
     }
 }
